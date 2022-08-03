33fd71771aab428c1d761c2838169e6c867e8084
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-08-04 00:34:45 +0900

    インストール用のスクリプトを追加

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -u
+
+BASEDIR=$PWD
+
+ln -snfv ${BASEDIR}/.vimrc ~/.vimrc
+ln -snfv ${BASEDIR}/.vim/dein.toml ~/.vim/dein.toml
+ln -snfv ${BASEDIR}/.vim/dein_lazy.toml ~/.vim/dein_lazy.toml
