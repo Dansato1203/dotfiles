bc30bd6bfa44e5c7a7bd38a61c3c0b462a575b01
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-09-10 15:12:19 +0900

    fix install.sh for 560b03e

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ BASEDIR=$PWD
 ln -snfv ${BASEDIR}/.vimrc ~/.vimrc
 ln -snfv ${BASEDIR}/.vim/dein.toml ~/.vim/dein.toml
 ln -snfv ${BASEDIR}/.vim/dein_lazy.toml ~/.vim/dein_lazy.toml
+
+source ${BASEDIR}/.bash/function.bash
