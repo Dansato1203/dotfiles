87144594005a81836c717977f0010729dcc1c6f9
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-09-10 17:13:00 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,3 @@ BASEDIR=$PWD
 ln -snfv ${BASEDIR}/.vimrc ~/.vimrc
 ln -snfv ${BASEDIR}/.vim/dein.toml ~/.vim/dein.toml
 ln -snfv ${BASEDIR}/.vim/dein_lazy.toml ~/.vim/dein_lazy.toml
-
-source ${BASEDIR}/.bash/function.bash
